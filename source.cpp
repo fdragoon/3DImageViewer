579aa456629ed5580d4e8b782d1db0764d83a410
fdragoon <taitan1hao@126.com>
2015-10-30 13:57:40 +0800

    v1.1 增加二维图像调试空间

--- source.cpp
+++ source.cpp
@@ -27,19 +27,29 @@ int main(int argc, char *argv[])
 	dicomReader->SetDirectoryName(dirname);
 	dicomReader->Update();
 
-	//创建显示窗口
-	vtkSmartPointer<vtkRenderWindow> windowsBasic = vtkSmartPointer<vtkRenderWindow> ::New();
-	windowsBasic->SetSize(500, 500);
+	////创建显示窗口
+	//vtkSmartPointer<vtkRenderWindow> windowsBasic = vtkSmartPointer<vtkRenderWindow> ::New();
+	//windowsBasic->SetSize(500, 500);
 
 	////创建vtkImageViewer3子窗口，用以显示二维图形
-	vtkSmartPointer<vtkImageViewer3> viewer = vtkSmartPointer<vtkImageViewer3>::New();
+	//vtkSmartPointer<vtkImageViewer3> viewer = vtkSmartPointer<vtkImageViewer3>::New();
+	////设置数据源
+	//viewer->SetInputConnection(dicomReader->GetOutputPort());
+	////设置显示坐标
+	//viewer->SetPoint(x, y, z);
+	////设置渲染窗口
+	//viewer->SetRenderWindow(windowsBasic);
+
+	//vtkImageViewer2单图像实现
+	vtkSmartPointer<vtkImageViewer2> viewer = vtkSmartPointer<vtkImageViewer2>::New();
 	viewer->SetInputConnection(dicomReader->GetOutputPort());
-	viewer->SetPoint(x, y, z);
-	viewer->SetRenderWindow(windowsBasic);
+	viewer->SetSliceOrientationToXY();
+	viewer->SetSlice(100);
 
 	//创建交互器
 	vtkSmartPointer<vtkRenderWindowInteractor>windowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	windowInteractor->SetRenderWindow(windowsBasic);
+	//windowInteractor->SetRenderWindow(windowsBasic);//-3
+	windowInteractor->SetRenderWindow(viewer->GetRenderWindow());//-2
 	//交互器样式暂时采用vtkInteractorStyleImage
 	vtkSmartPointer<vtkInteractorStyleImage>windowInteractorStyle = vtkSmartPointer<vtkInteractorStyleImage>::New();
 	windowInteractor->SetInteractorStyle(windowInteractorStyle);
